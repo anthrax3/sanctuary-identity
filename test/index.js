951662b9c0ad94df3b3eb19001a79f9ac8d90dc6
David Chambers <dc@davidchambers.me>
2018-04-11 11:50:31 +0200

    curry ‘eq’

--- test/index.js
+++ test/index.js
@@ -9,11 +9,14 @@ var Z = require('sanctuary-type-classes');
 var Identity = require('..');
 
 
-//  eq :: (Any, Any) -> Undefined !
-function eq(actual, expected) {
-  assert.strictEqual(arguments.length, eq.length);
-  assert.strictEqual(Z.toString(actual), Z.toString(expected));
-  assert.strictEqual(Z.equals(actual, expected), true);
+//  eq :: a -> b -> Undefined !
+function eq(actual) {
+  assert.strictEqual (arguments.length, eq.length);
+  return function eq$1(expected) {
+    assert.strictEqual (arguments.length, eq$1.length);
+    assert.strictEqual (Z.toString (actual), Z.toString (expected));
+    assert.strictEqual (Z.equals (actual, expected), true);
+  };
 }
 
 //  IdentityArb :: Arbitrary a -> Arbitrary (Identity a)
@@ -35,55 +38,55 @@ function value(r) {
 
 
 test('equals', function() {
-  eq(Z.equals(Identity([1, 2, 3]), Identity([1, 2, 3])), true);
-  eq(Z.equals(Identity([1, 2, 3]), Identity([3, 2, 1])), false);
+  eq(Z.equals(Identity([1, 2, 3]), Identity([1, 2, 3])))(true);
+  eq(Z.equals(Identity([1, 2, 3]), Identity([3, 2, 1])))(false);
 });
 
 test('lte', function() {
-  eq(Z.lte(Identity(0), Identity(0)), true);
-  eq(Z.lte(Identity(0), Identity(1)), true);
-  eq(Z.lte(Identity(1), Identity(0)), false);
+  eq(Z.lte(Identity(0), Identity(0)))(true);
+  eq(Z.lte(Identity(0), Identity(1)))(true);
+  eq(Z.lte(Identity(1), Identity(0)))(false);
 });
 
 test('concat', function() {
-  eq(Z.concat(Identity([1, 2]), Identity([3, 4])), Identity([1, 2, 3, 4]));
+  eq(Z.concat(Identity([1, 2]), Identity([3, 4])))(Identity([1, 2, 3, 4]));
 });
 
 test('map', function() {
-  eq(Z.map(Math.sqrt, Identity(9)), Identity(3));
+  eq(Z.map(Math.sqrt, Identity(9)))(Identity(3));
 });
 
 test('ap', function() {
-  eq(Z.ap(Identity(Math.sqrt), Identity(9)), Identity(3));
+  eq(Z.ap(Identity(Math.sqrt), Identity(9)))(Identity(3));
 });
 
 test('chain', function() {
-  eq(Z.chain(compose(Identity, Math.sqrt), Identity(9)), Identity(3));
+  eq(Z.chain(compose(Identity, Math.sqrt), Identity(9)))(Identity(3));
 });
 
 test('alt', function() {
-  eq(Z.alt(Identity([1, 2]), Identity([3, 4])), Identity([1, 2, 3, 4]));
+  eq(Z.alt(Identity([1, 2]), Identity([3, 4])))(Identity([1, 2, 3, 4]));
 });
 
 test('reduce', function() {
-  eq(Z.reduce(Z.concat, [1, 2], Identity([3, 4])), [1, 2, 3, 4]);
+  eq(Z.reduce(Z.concat, [1, 2], Identity([3, 4])))([1, 2, 3, 4]);
 });
 
 test('traverse', function() {
-  eq(Z.traverse(Array, function(s) { return s.split(''); }, Identity('abc')),
-     [Identity('a'), Identity('b'), Identity('c')]);
+  eq(Z.traverse(Array, function(s) { return s.split(''); }, Identity('abc')))
+    ([Identity('a'), Identity('b'), Identity('c')]);
 });
 
 test('extend', function() {
-  eq(Z.extend(compose(Math.sqrt, value), Identity(9)), Identity(3));
+  eq(Z.extend(compose(Math.sqrt, value), Identity(9)))(Identity(3));
 });
 
 test('extract', function() {
-  eq(Z.extract(Identity(0)), 0);
+  eq(Z.extract(Identity(0)))(0);
 });
 
 test('toString', function() {
-  eq(Z.toString(Identity([1, 2, 3])), 'Identity([1, 2, 3])');
+  eq(Z.toString(Identity([1, 2, 3])))('Identity([1, 2, 3])');
 });
 
 suite('Comonad laws', function() {
