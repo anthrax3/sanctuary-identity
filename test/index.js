482da54cb5c8cad3063542d54e9443809045e196
David Chambers <dc@davidchambers.me>
2018-04-30 10:50:39 +0200

    reformat test suite

--- test/index.js
+++ test/index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var assert = require('assert');
+const assert = require ('assert');
 
-var laws = require('fantasy-laws');
-var jsc = require('jsverify');
-const show = require('sanctuary-show');
-var Z = require('sanctuary-type-classes');
+const laws = require ('fantasy-laws');
+const jsc = require ('jsverify');
+const show = require ('sanctuary-show');
+const Z = require ('sanctuary-type-classes');
 
-var Identity = require('..');
+const Identity = require ('..');
 
 
-//  eq :: a -> b -> Undefined !
+//    eq :: a -> b -> Undefined !
 function eq(actual) {
   assert.strictEqual (arguments.length, eq.length);
   return function eq$1(expected) {
@@ -20,91 +20,83 @@ function eq(actual) {
   };
 }
 
-//  IdentityArb :: Arbitrary a -> Arbitrary (Identity a)
-function IdentityArb(arb) {
-  return arb.smap(Identity, value, show);
-}
+//    IdentityArb :: Arbitrary a -> Arbitrary (Identity a)
+const IdentityArb = arb => arb.smap (Identity, value, show);
 
-//  compose :: (b -> c, a -> b) -> a -> c
-function compose(f, g) {
-  return function(x) {
-    return f(g(x));
-  };
-}
+//    compose :: (b -> c) -> (a -> b) -> a -> c
+const compose = f => g => x => f (g (x));
 
-//  value :: { value :: a } -> a
-function value(r) {
-  return r.value;
-}
+//    value :: { value :: a } -> a
+const value = r => r.value;
 
 
-test('equals', function() {
-  eq(Z.equals(Identity([1, 2, 3]), Identity([1, 2, 3])))(true);
-  eq(Z.equals(Identity([1, 2, 3]), Identity([3, 2, 1])))(false);
+test ('equals', () => {
+  eq (Z.equals (Identity ([1, 2, 3]), Identity ([1, 2, 3]))) (true);
+  eq (Z.equals (Identity ([1, 2, 3]), Identity ([3, 2, 1]))) (false);
 });
 
-test('lte', function() {
-  eq(Z.lte(Identity(0), Identity(0)))(true);
-  eq(Z.lte(Identity(0), Identity(1)))(true);
-  eq(Z.lte(Identity(1), Identity(0)))(false);
+test ('lte', () => {
+  eq (Z.lte (Identity (0), Identity (0))) (true);
+  eq (Z.lte (Identity (0), Identity (1))) (true);
+  eq (Z.lte (Identity (1), Identity (0))) (false);
 });
 
-test('concat', function() {
-  eq(Z.concat(Identity([1, 2]), Identity([3, 4])))(Identity([1, 2, 3, 4]));
+test ('concat', () => {
+  eq (Z.concat (Identity ([1, 2]), Identity ([3, 4]))) (Identity ([1, 2, 3, 4]));
 });
 
-test('map', function() {
-  eq(Z.map(Math.sqrt, Identity(9)))(Identity(3));
+test ('map', () => {
+  eq (Z.map (Math.sqrt, Identity (9))) (Identity (3));
 });
 
-test('ap', function() {
-  eq(Z.ap(Identity(Math.sqrt), Identity(9)))(Identity(3));
+test ('ap', () => {
+  eq (Z.ap (Identity (Math.sqrt), Identity (9))) (Identity (3));
 });
 
-test('of', function() {
-  eq(Z.of(Identity, 42))(Identity(42));
+test ('of', () => {
+  eq (Z.of (Identity, 42)) (Identity (42));
 });
 
-test('chain', function() {
-  eq(Z.chain(compose(Identity, Math.sqrt), Identity(9)))(Identity(3));
+test ('chain', () => {
+  eq (Z.chain (compose (Identity) (Math.sqrt), Identity (9))) (Identity (3));
 });
 
-test('alt', function() {
-  eq(Z.alt(Identity([1, 2]), Identity([3, 4])))(Identity([1, 2, 3, 4]));
+test ('alt', () => {
+  eq (Z.alt (Identity ([1, 2]), Identity ([3, 4]))) (Identity ([1, 2, 3, 4]));
 });
 
-test('reduce', function() {
-  eq(Z.reduce(Z.concat, [1, 2], Identity([3, 4])))([1, 2, 3, 4]);
+test ('reduce', () => {
+  eq (Z.reduce (Z.concat, [1, 2], Identity ([3, 4]))) ([1, 2, 3, 4]);
 });
 
-test('traverse', function() {
-  eq(Z.traverse(Array, function(s) { return s.split(''); }, Identity('abc')))
-    ([Identity('a'), Identity('b'), Identity('c')]);
+test ('traverse', () => {
+  eq (Z.traverse (Array, s => s.split (''), Identity ('abc')))
+     ([Identity ('a'), Identity ('b'), Identity ('c')]);
 });
 
-test('extend', function() {
-  eq(Z.extend(compose(Math.sqrt, value), Identity(9)))(Identity(3));
+test ('extend', () => {
+  eq (Z.extend (compose (Math.sqrt) (value), Identity (9))) (Identity (3));
 });
 
-test('extract', function() {
-  eq(Z.extract(Identity(0)))(0);
+test ('extract', () => {
+  eq (Z.extract (Identity (0))) (0);
 });
 
-test('@@show', function() {
-  eq(show(Identity([1, 2, 3])))('Identity ([1, 2, 3])');
+test ('@@show', () => {
+  eq (show (Identity ([1, 2, 3]))) ('Identity ([1, 2, 3])');
 });
 
-suite('Comonad laws', function() {
+suite ('Comonad laws', () => {
 
-  test('left identity',
-       laws.Comonad(Z.equals).leftIdentity(
-         IdentityArb(jsc.integer)
-       ));
+  test ('left identity',
+        (laws.Comonad (Z.equals)).leftIdentity (
+          IdentityArb (jsc.integer)
+        ));
 
-  test('right identity',
-       laws.Comonad(Z.equals).rightIdentity(
-         IdentityArb(jsc.integer),
-         jsc.constant(function(identity) { return identity.value + 1; })
-       ));
+  test ('right identity',
+        (laws.Comonad (Z.equals)).rightIdentity (
+          IdentityArb (jsc.integer),
+          jsc.constant (identity => identity.value + 1)
+        ));
 
 });
